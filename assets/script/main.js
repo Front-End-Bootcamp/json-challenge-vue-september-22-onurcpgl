03d7dee452caee3a121097ec59ebf000dd89f24f
Onur Çöpoğlu <onurcpgl.52@gmail.com>
2022-09-15 10:14:42 +0300

    homework-1

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,1 +1,32 @@
-console.log('👻');
+import axios from "axios";
+
+const getData = async () => {
+	const data = await axios.get("./assets/data").then((res) => res.data);
+	return data;
+};
+
+const filterByGroupName = async (name) => {
+	const response = await getData();
+
+	const groupByCategory = {
+		[name]: response
+			.filter((x) => x.group == name)
+			.reduce((finalObject, person) => {
+				const { type } = person;
+
+				if (type == "assistant") {
+					finalObject[type] = person.name;
+				} else {
+					finalObject["student"] = finalObject["student"] ?? [];
+					finalObject["student"].push(person.name);
+				}
+
+				return finalObject;
+			}, {}),
+	};
+
+	console.log(groupByCategory);
+	return response;
+};
+
+filterByGroupName("YellowGreen");
